cdf92cff33de8046a2f64dd161656d8d73146f83
James Howard <jameshoward@mac.com>
2016-01-14 16:53:21 -0800

    move stuff around to be able to become a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'shippy',
+  packages = ['shippy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python interface to the Ship issue tracker',
+  author = 'James Howard',
+  author_email = 'james@realartists.com',
+  url = 'https://github.com/realartists/shippy',
+  download_url = 'https://github.com/realartists/shippy/tarball/0.1',
+  keywords = [], # arbitrary keywords
+  classifiers = [],
+)
