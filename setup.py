8b3950ea9d32b3168902d78c616e5cd5e9fa0f5b
James Howard <jameshoward@mac.com>
2016-01-14 17:23:40 -0800

    update version #

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'shippy',
   packages = ['shippy'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'A python interface to the Ship issue tracker',
   author = 'James Howard',
   author_email = 'james@realartists.com',
   url = 'https://github.com/realartists/shippy',
-  download_url = 'https://github.com/realartists/shippy/tarball/0.1',
+  download_url = 'https://github.com/realartists/shippy/tarball/0.1.1',
   keywords = [], # arbitrary keywords
   classifiers = [],
   install_requires=['requests']
